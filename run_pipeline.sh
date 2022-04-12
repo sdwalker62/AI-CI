f57579825946fbdce95e5021180c3ad909123d2e
Dalton Walker <dalton_walker@icloud.com>
2022-04-12 13:11:26 -0400

    dasd

--- run_pipeline.sh
+++ run_pipeline.sh
@@ -10,7 +10,7 @@ clear='\033[0m'
 echo "============================="
 echo "${blue}RUNNING AI CI PIPELINE${clear}"
 
-if [[ -f "Dockerfile.ci" ]];
+if [[ -f "./Dockerfile.ci" ]];
 then
     docker build -t ci_pipeline -f Dockerfile.ci .
     docker run -v `pwd`/coverage/:/coverage/ ci_pipeline
