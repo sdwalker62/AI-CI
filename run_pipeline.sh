2ae373c2947331735299c555d93295630d26c559
Dalton Walker <dalton_walker@icloud.com>
2022-04-12 13:23:06 -0400

    why u no work

--- run_pipeline.sh
+++ run_pipeline.sh
@@ -12,11 +12,11 @@ echo "${blue}RUNNING AI CI PIPELINE${clear}"
 echo "${blue}======================${clear}"
 
 
-if [[ -f "./Dockerfile.ci" ]]; then
-    docker build -t ci_pipeline -f Dockerfile.ci .
-    docker run -v `pwd`/coverage/:/coverage/ ci_pipeline
-    scp -i "sonarqube.pem" coverage/converage.xml ubuntu@ec2-44-196-133-163.compute-1.amazonaws.com:/home/ubuntu/sonarqube/conf/
-    echo "${green}FINISH${clear}"
-else
-    echo "No Dockerfile found!"
-fi 
+# if [[ -f "./Dockerfile.ci" ]]; then
+docker build -t ci_pipeline -f Dockerfile.ci .
+docker run -v `pwd`/coverage/:/coverage/ ci_pipeline
+scp -i "sonarqube.pem" coverage/converage.xml ubuntu@ec2-44-196-133-163.compute-1.amazonaws.com:/home/ubuntu/sonarqube/conf/
+echo "${green}FINISH${clear}"
+# else
+#     echo "No Dockerfile found!"
+# fi 
