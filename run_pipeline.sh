f618070e8b90ca9d223799c6086fcb68ca211646
Dalton Walker <dalton_walker@icloud.com>
2022-04-12 13:15:24 -0400

    ..

--- run_pipeline.sh
+++ run_pipeline.sh
@@ -7,15 +7,16 @@ blue='\033[1;34m'
 clear='\033[0m'
 
 
-echo "============================="
+echo "${blue}======================${clear}"
 echo "${blue}RUNNING AI CI PIPELINE${clear}"
+echo "${blue}======================${clear}"
 
-if [[ -f "./Dockerfile.ci" ]];
-then
+
+if [[ -f "./Dockerfile.ci" ]]; then
     docker build -t ci_pipeline -f Dockerfile.ci .
     docker run -v `pwd`/coverage/:/coverage/ ci_pipeline
-    scp -i "sonarqube.pem" coverage/index.html ubuntu@ec2-44-196-133-163.compute-1.amazonaws.com:/var/www/html/
-    echo "${green}OK${clear}"
+    # scp -i "sonarqube.pem" coverage/index.html ubuntu@ec2-44-196-133-163.compute-1.amazonaws.com:/var/www/html/
+    echo "${green}FINISH${clear}"
 else
     echo "No Dockerfile found!"
 fi 
