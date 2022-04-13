f3ee95b37cb822fa8aa5733635f4eae4ee3f6705
Andrew Romans <andrewromans@wsip-70-175-234-35.mc.at.cox.net>
2022-04-13 14:54:41 -0400

    Gold 011

--- ci_pipeline.py
+++ ci_pipeline.py
@@ -79,7 +79,7 @@ if os.path.isfile('./Dockerfile.ci'):
     ci_container = client.containers.run(
         image='ai_ci', 
         detach=True,
-        volumes=['./.github/workflows:/home/ubuntu/coverage/']
+        volumes=['/Users/andrewromans/Dev/Work/AI-CI/.github/workflows:/home/ubuntu/coverage']
     )
     process = ci_container.logs(stream=True, follow=True)
     for line in process:
