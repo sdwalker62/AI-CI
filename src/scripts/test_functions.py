88ba0deceb14a869f2e17fb950bd4c63dc893ef9
Dalton Walker <samuel.dalton.walker@outlook.com>
2022-04-18 09:49:17 -0400

    Adds division func

--- src/scripts/test_functions.py
+++ src/scripts/test_functions.py
@@ -2,8 +2,8 @@ def add_one(x):
     """
     Adds one to a number and returns the result.
 
-    :param kind: Number to incremented
-    :type kind: int
+    :param x: Number to incremented
+    :type x: int
     :raise ValueError: if x is non-numeric
     :return: incremented value
     :rtype: int
@@ -15,8 +15,8 @@ def subtract_one(x):
     """
     Subtracts one to a number and returns the result.
 
-    :param kind: Number to decremented
-    :type kind: int
+    :param x: Number to decremented
+    :type x: int
     :raise ValueError: if x is non-numeric
     :return: decremented value
     :rtype: int
@@ -28,10 +28,56 @@ def multiply_one(x):
     """
     Multiplies a number by one and returns the result.
 
-    :param kind: Number to be multiplied
-    :type kind: int
+    :param x: Number to be multiplied
+    :type x: int
     :raise ValueError: if x is non-numeric
-    :return: decremented value
+    :return: multiplied value
     :rtype: int
     """
+    return x * 1
+
+
+
+
+
+
+
+
+def divide_one(x):
+    """
+    Divides a number by one and returns the result.
+
+    :param x: Number to be divided
+    :type x: int
+    :raise ValueError: if x is non-numeric
+    :return: divided value
+    :rtype: int
+    """
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     return x * 1
