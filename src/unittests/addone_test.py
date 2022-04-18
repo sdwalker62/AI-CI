632b57d464e78faa33581719d51580986a654217
Andrew Romans <andrewromans@Andrews-MacBook-Pro.local>
2022-04-18 17:03:57 -0400

    Adding unit tests

--- src/unittests/addone_test.py
+++ src/unittests/addone_test.py
@@ -4,7 +4,7 @@ import os
 # Append main directory
 sys.path.insert(1, os.path.join(sys.path[0], ".."))
 
-from scripts.test_functions import add_one, subtract_one
+from scripts.test_functions import add_one, subtract_one, divide_one, multiply_one
 
 import unittest
 
@@ -15,6 +15,12 @@ class MathTests(unittest.TestCase):
 
     def test_subtract_one(self):
         self.assertEqual(subtract_one(155), 154)
+
+    def test_multiply_one(self):
+        self.assertEqual(multiply_one(1337), 1337)
+
+    def test_divide_one(self):
+        self.assertEqual(divide_one(31), 31)
 
 
 """
