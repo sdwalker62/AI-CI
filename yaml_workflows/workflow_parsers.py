e29d5584923f0608886a3e1a58f7c68dfb9aa55b
Andrew Romans <andrewromans@Andrews-MacBook-Pro.local>
2022-04-20 15:32:52 -0400

    Using a better yaml library.. I hate yaml

--- yaml_workflows/workflow_parsers.py
+++ yaml_workflows/workflow_parsers.py
@@ -1,95 +1,14 @@
-import yaml
+from ruamel.yaml import YAML
+
+yaml = YAML()
 
 with open("discord_messages.yml", "r") as stream:
-    discord_messages = yaml.safe_load(stream)
-
-# Parse header sections for pipeline yaml
-def parse_headers(pipeline, indent=0):
-    line = ""
-    if (type(pipeline) == dict):
-        for item, doc in pipeline.items():
-            for _ in range(0, indent):
-                line += '\t'
-
-            if (item == True):
-                item = 'on'
-
-            if (item == 'cron'):
-                line += f"- {item}: "
-            else:
-                line += f"{item}: "
-
-            if (type(doc) != dict and type(doc) != list):
-                if (item == 'cron'):
-                    line += f"\'{doc}\'\n"
-                else:
-                    line += f"{doc}\n"
-            else:
-                if (type(doc) == list):
-                    if (type(doc[0]) != dict):
-                        for branch in doc:
-                            line += "\n"
-                            for _ in range(indent):
-                                line += "\t"
-                            line += f"\t- \'{branch}\'\n"
-                        continue
-                    else:
-                        doc = doc[0]
-                line += "\n"
-                line += parse_headers(doc, indent=indent+1) 
-    else:
-        print(pipeline)
-        if (type(pipeline) == list):
-            for itm in pipeline:
-                for _ in range(0, indent):
-                    line += '\t'
-                line += f" - \'{itm}\'\n"
-        line += "\n"
-
-    line = line.replace('\t', ' ')
-    return line
-
-# Parse functionality yaml files for pipeline yaml
-def parse_functions(pipeline, indent=2):
-    line = ""
-    if (type(pipeline) == dict):
-        for item, doc in pipeline.items():
-
-            for _ in range(indent):
-                line += "\t"
-
-            if "name" == item:
-                line += f"- {item}: "
-            else:
-                line += f"\t{item}: "
-
-            if (type(doc) != dict):
-                if (item == "run"):
-                    run_items = doc.split('\n')
-                    if (len(run_items) > 1):
-                        for run_itm in run_items:
-                            if (run_itm == ''): 
-                                line += "\n"
-                                continue
-
-                            line += " |\n"
-                            for _ in range(indent):
-                                line += "\t"
-                            line += f"\t\t{run_itm}"
-                    else:
-                        line += f"{doc}\n"
-                else:
-                    line += f"{doc}\n"
-            else:
-                line += "\n"
-                line += parse_functions(doc, indent=indent+1)
-
-    line = line.replace('\t', ' ')
-    return line
+    discord_messages = yaml.load(stream)
 
 # Parse steps (functionality combination) for pipeline yaml
 def parse_steps(steps):
-    lines = []
+    step_dict = {'steps': []}
+
     skip_message = False
     for j in range(len(steps)):
         if (skip_message):
@@ -98,19 +17,14 @@ def parse_steps(steps):
 
         s = steps[j]
 
-        file = f"pipeline_functions/{s}.yml"
+        file = f"./pipeline_functions/{s}.yml"
         with open(file, "r") as stream:
-            pipeline = yaml.safe_load(stream)
+            pipeline = yaml.load(stream)
 
         if "discord_notifier" == s:
             pipeline[0]["with"]["args"] = discord_messages[steps[j+1]]
             skip_message = True
-        
-        line = ""
-        for i in range(len(pipeline)):
-            line += parse_functions(pipeline[i], indent=3)
-            line += "\n"
-        
-        lines.append(line)
 
-    return lines
+        step_dict['steps'].append(pipeline[0])
+
+    return step_dict
--- yaml_workflows/worklow_compiler.py
+++ yaml_workflows/worklow_compiler.py
@@ -1,17 +1,15 @@
 #!/usr/bin/env python
 
-import yaml
-from workflow_parsers import parse_headers, parse_steps
+from ruamel.yaml import YAML
+from workflow_parsers import parse_steps
 from pipeline_steps import commit_steps, merge_steps, pr_nightly_steps
 
 file = {}
 
-
-with open("discord_messages.yml", "r") as stream:
-    discord_messages = yaml.safe_load(stream)
+yaml = YAML()
     
 with open("workflow_headers.yml", "r") as stream:
-    file = yaml.safe_load(stream)
+    file = yaml.load(stream)
 
 pipeline_files = ["commit-pipeline", "merge-pipeline", "pr_nightly-pipeline"]
 
@@ -42,55 +40,21 @@ else:
     steps = [commit_steps, merge_steps, pr_nightly_steps]
     x = [0, 1, 2]
 
-print(steps)
-print(x)
-
 for i in range(0, len(steps)):
     p = pipeline_files[x[i]]
-    print(p)
 
     workflow = open(f'../.github/workflows/{p}.yaml', 'w')
 
     workflow_pipeline = file[p]
 
-    # documents = yaml.dump(pipeline, workflow)
-
-    step_dict = {'steps': []}
-
-    skip_message = False
-    for j in range(len(steps[i])):
-        if (skip_message):
-            skip_message = False
-            continue
-
-        s = steps[i][j]
-        print(s)
-
-        file = f"./pipeline_functions/{s}.yml"
-        with open(file, "r") as stream:
-            pipeline = yaml.safe_load(stream)
-
-        if "discord_notifier" == s:
-            pipeline[0]["with"]["args"] = discord_messages[steps[i][j+1]]
-            skip_message = True
-
-        step_dict['steps'].append(pipeline[0])
+    step_documents = parse_steps(steps[i])
     
-    print(workflow_pipeline)
     if "jobs" in workflow_pipeline:
-        print(workflow_pipeline.keys())
         keyList = list(workflow_pipeline['jobs'].keys())
-        workflow_pipeline['jobs'][keyList[0]]['steps'] = step_dict['steps']
+        workflow_pipeline['jobs'][keyList[0]]['steps'] = step_documents['steps']
 
+    # Write compiled yaml file
     documents = yaml.dump(workflow_pipeline, workflow)
-        
-
-
-    # Headers
-    # workflow.writelines(parse_headers(pipeline))
-
-    # # Steps
-    # workflow.writelines(parse_steps(steps[i]))
 
     # Close File
     workflow.close()
